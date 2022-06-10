3dd5b5b1f5eaa38807a4127c6de8419385066b18
MCLegoMan <46882277+MCLegoMan@users.noreply.github.com>
2022-06-11 00:33:39 +1200

    Added Blocks Item Group, Added Language Assets.

--- src/main/java/com/mclegoman/voyager/registry/ModItemGroup.java
+++ src/main/java/com/mclegoman/voyager/registry/ModItemGroup.java
@@ -8,6 +8,11 @@ import net.minecraft.util.Identifier;
 import static com.mclegoman.voyager.Main.MOD_ID;
 
 public class ModItemGroup {
+
     public static final ItemGroup VOYAGER_ITEMS = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "voyager_items"),
             () -> new ItemStack(ModItems.INFUSED_GEM));
+
+    public static final ItemGroup VOYAGER_BLOCKS = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "voyager_blocks"),
+            () -> new ItemStack(ModBlocks.INFUSED_GEM_BLOCK));
+
 }
