e858b154920bd5b7d62617d56535c23ea3785b87
MCLegoMan <46882277+MCLegoMan@users.noreply.github.com>
2022-06-11 00:27:50 +1200

    Added Infused Gem Block, Added Infused Gem Block Assets, Updated Infused Gem and Infused Gem Shard Texture Assets.

--- src/main/java/com/mclegoman/voyager/Main.java
+++ src/main/java/com/mclegoman/voyager/Main.java
@@ -1,6 +1,7 @@
 package com.mclegoman.voyager;
 
 import com.mclegoman.voyager.enchantments.ModEnchantments;
+import com.mclegoman.voyager.registry.ModBlocks;
 import com.mclegoman.voyager.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -13,6 +14,7 @@ public class Main implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 		//ModLootTableModifiers.modifyLootTables();
 		ModEnchantments.registerModEnchantments();
 	}
